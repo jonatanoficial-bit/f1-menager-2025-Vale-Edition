c608bcec906423dded89225f0b6b47704247d7f4
jonatanoficial-bit <jonatanoficial@gmail.com>
2025-12-20 10:40:16 -0300

    Add files via upload

---
+++ track-test.js
@@ -0,0 +1,6 @@
+
+// track-test.js
+console.log("Validador de pistas carregado corretamente.");
+
+// ESTE ARQUIVO É UM PLACEHOLDER SIMPLES
+// Ele serve para confirmar que o GitHub Pages está servindo arquivos corretamente.
